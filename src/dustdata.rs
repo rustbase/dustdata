ec8edcfd525a9e4c541259056b53f143ff600701
pedrinfx <canaldopedrinho233@gmail.com>
2022-08-21 16:37:13 -0300

    remove: cache

--- src/dustdata.rs
+++ src/dustdata.rs
@@ -1,7 +1,7 @@
 use super::storage;
 
+use std::ops::Add;
 use std::path;
-use std::sync::{Arc, Mutex};
 use storage::lsm;
 
 /// A LSM configuration
@@ -9,7 +9,7 @@ use storage::lsm;
 /// * `flush_threshold` - The number of bytes to flush before flushing to disk
 #[derive(Clone)]
 pub struct LsmConfig {
-    pub flush_threshold: usize,
+    pub flush_threshold: Size,
 }
 
 /// A DustData configuration
@@ -19,30 +19,75 @@ pub struct LsmConfig {
 /// * `lsm_config` - The LSM configuration
 #[derive(Clone)]
 pub struct DustDataConfig {
-    pub cache_size: usize,
     pub path: String,
     pub lsm_config: LsmConfig,
 }
 
 pub struct DustData {
     pub config: DustDataConfig,
-    pub cache: Arc<Mutex<super::cache::Cache>>,
     pub lsm: storage::lsm::Lsm,
+}
+
+#[derive(Clone)]
+pub enum Size {
+    Bytes(usize),
+    Kilobytes(usize),
+    Megabytes(usize),
+    Gigabytes(usize),
+}
+
+impl Add for Size {
+    type Output = Size;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        match self {
+            Size::Bytes(lhs) => match rhs {
+                Size::Bytes(rhs) => Size::Bytes(lhs + rhs),
+                Size::Kilobytes(rhs) => Size::Bytes(lhs + (rhs * 1024)),
+                Size::Megabytes(rhs) => Size::Bytes(lhs + (rhs * 1024 * 1024)),
+                Size::Gigabytes(rhs) => Size::Bytes(lhs + (rhs * 1024 * 1024 * 1024)),
+            },
+            Size::Kilobytes(lhs) => match rhs {
+                Size::Bytes(rhs) => Size::Bytes(lhs + (rhs * 1024)),
+                Size::Kilobytes(rhs) => Size::Kilobytes(lhs + rhs),
+                Size::Megabytes(rhs) => Size::Kilobytes(lhs + (rhs * 1024)),
+                Size::Gigabytes(rhs) => Size::Kilobytes(lhs + (rhs * 1024 * 1024)),
+            },
+            Size::Megabytes(lhs) => match rhs {
+                Size::Bytes(rhs) => Size::Bytes(lhs + (rhs * 1024 * 1024)),
+                Size::Kilobytes(rhs) => Size::Bytes(lhs + (rhs * 1024 * 1024 * 1024)),
+                Size::Megabytes(rhs) => Size::Megabytes(lhs + rhs),
+                Size::Gigabytes(rhs) => Size::Megabytes(lhs + (rhs * 1024)),
+            },
+            Size::Gigabytes(lhs) => match rhs {
+                Size::Bytes(rhs) => Size::Bytes(lhs + (rhs * 1024 * 1024 * 1024)),
+                Size::Kilobytes(rhs) => Size::Bytes(lhs + (rhs * 1024 * 1024 * 1024 * 1024)),
+                Size::Megabytes(rhs) => Size::Bytes(lhs + (rhs * 1024 * 1024 * 1024 * 1024 * 1024)),
+                Size::Gigabytes(rhs) => Size::Gigabytes(lhs + rhs),
+            },
+        }
+    }
+}
+
+pub fn parse_size(size: Size) -> usize {
+    match size {
+        Size::Bytes(bytes) => bytes,
+        Size::Kilobytes(kilobytes) => kilobytes * 1024,
+        Size::Megabytes(megabytes) => megabytes * 1024 * 1024,
+        Size::Gigabytes(gigabytes) => gigabytes * 1024 * 1024 * 1024,
+    }
 }
 
 impl DustData {
     pub fn new(configuration: DustDataConfig) -> Self {
         let path = path::Path::new(&configuration.path);
 
-        let cache = super::cache::Cache::new_app_cache(configuration.cache_size);
-
         let lsm = storage::lsm::Lsm::new(lsm::LsmConfig {
-            flush_threshold: configuration.lsm_config.flush_threshold,
+            flush_threshold: parse_size(configuration.clone().lsm_config.flush_threshold),
             sstable_path: path.to_str().unwrap().to_string(),
         });
 
         Self {
-            cache,
             lsm,
             config: configuration,
         }
@@ -54,23 +99,7 @@ impl DustData {
     /// # Returns
     /// - `Some(bson::Document)` if value was found returns a bson document
     pub fn get(&mut self, key: &str) -> Option<bson::Document> {
-        let cache = self.cache.lock().unwrap();
-        let document = cache.get(key);
-
-        if let Some(document) = document {
-            return Some(document.result.as_document().unwrap().clone());
-        }
-
-        let document = self.lsm.get(key);
-
-        if document.is_some() {
-            self.cache.lock().unwrap().add(
-                key.to_string(),
-                bson::Bson::Document(document.as_ref().unwrap().clone()),
-            );
-        }
-
-        document
+        self.lsm.get(key)
     }
 
     /// Insert a value with a key
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 mod bloom;
-mod cache;
 mod dustdata;
 mod storage;
 
-pub use self::{dustdata::DustData, dustdata::DustDataConfig, dustdata::LsmConfig};
+pub use self::{dustdata::DustData, dustdata::DustDataConfig, dustdata::LsmConfig, dustdata::Size};
 
 /// Initialize the database
 /// # Arguments
@@ -12,4 +11,31 @@ pub use self::{dustdata::DustData, dustdata::DustDataConfig, dustdata::LsmConfig
 /// - DustData object
 pub fn initialize(config: dustdata::DustDataConfig) -> dustdata::DustData {
     dustdata::DustData::new(config)
+}
+
+#[cfg(test)]
+mod dustdata_tests {
+    use super::*;
+
+    #[test]
+    fn insert_document() {
+        let config = DustDataConfig {
+            path: "./test_data".to_string(),
+            lsm_config: LsmConfig {
+                flush_threshold: Size::Megabytes(128),
+            },
+        };
+
+        let mut dd = initialize(config);
+
+        dd.insert(
+            "test",
+            bson::doc! {
+                "test": "test"
+            },
+        )
+        .unwrap();
+
+        assert!(dd.get("test").is_some());
+    }
 }
