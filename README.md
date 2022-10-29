d21342bb47cf8c940681e4f032202d9613878d4d
pedrinfx <canaldopedrinho233@gmail.com>
2022-10-29 15:23:03 -0300

    feat: lzzzz to lz4

--- README.md
+++ README.md
@@ -1,30 +1,38 @@
 [![crates.io](https://img.shields.io/crates/v/dustdata?color=EA4342&style=flat-square)](https://crates.io/crates/dustdata)
 
-
 # DustData
+
 A data concurrency control storage engine to [Rustbase](https://github.com/rustbase/rustbase)
 
 Join our [community](https://discord.gg/m5ZzWPumbd) and [chat](https://discord.gg/m5ZzWPumbd) with other Rust users.
 
-# ⚠️ Warning 
+# ⚠️ Warning
+
 This is a work in progress. The API is not stable yet.
 
 # 🔗 Contribute
+
 [Click here](./CONTRIBUTING.md) to see how to Contribute
 
 # Dependencies
+
 These are dependencies that are required to use the DustData.
- - [bson](https://crates.io/crates/bson)
+
+-   [bson](https://crates.io/crates/bson)
 
 # How to install
+
 Add the following to your `Cargo.toml`:
+
 ```toml
 [dependencies]
-dustdata = "0.4.3"
+dustdata = "0.4.4"
 ```
 
 # Usage
+
 Initialize a DustData interface.
+
 ```rust
 // DustData Configuration
 let config = dustdata::DustDataConfig {
@@ -33,11 +41,12 @@ let config = dustdata::DustDataConfig {
         flush_threshold: dustdata::Size::Megabytes(128),
     }
 };
-        
+
 let mut dustdata = dustdata::initialize(config);
 ```
 
 ## Insert a data
+
 ```rust
 // ...
 // Creating a data
@@ -50,6 +59,7 @@ dustdata.insert("key", data);
 ```
 
 ## Getting a data
+
 ```rust
 // ...
 let value = dustdata.get("key").unwrap().unwrap();
@@ -57,6 +67,7 @@ println!("{:?}", value);
 ```
 
 ## Updating a data
+
 ```rust
 // ...
 let data = bson::doc! {
@@ -68,23 +79,28 @@ dustdata.update("key", data);
 ```
 
 ## Deleting a data
+
 ```rust
 // ...
 dustdata.delete("key");
 ```
 
 # To-dos
- - [x] Memtable (06/19/22)
- - [x] SSTable (08/20/22)
- - [ ] Logs
- - [ ] Encryption
+
+-   [x] Memtable (06/19/22)
+-   [x] SSTable (08/20/22)
+-   [ ] Logs
+-   [ ] Encryption
 
 # Authors
+
 <div align="center">
 
 | [<img src="https://github.com/pedrinfx.png?size=115" width=115><br><sub>@pedrinfx</sub>](https://github.com/pedrinfx) |
 | :-------------------------------------------------------------------------------------------------------------------: |
+
 </div>
 
 # License
+
 [MIT License](./LICENSE)
--- src/storage/lsm/filter/mod.rs
+++ src/storage/lsm/filter/mod.rs
@@ -1,4 +1,5 @@
 use crate::bloom::BloomFilter;
+use lz4::{Decoder, EncoderBuilder};
 use std::io::{Read, Write};
 
 pub fn check_if_filter_exists(path: &str) -> bool {
@@ -14,21 +15,23 @@ pub fn write_filter(path: &str, filter: &BloomFilter) {
         std::fs::create_dir_all(_path.clone()).unwrap();
     }
 
-    let mut bitvec_comp = Vec::new();
-    lzzzz::lz4f::compress_to_vec(
-        &filter.bitvec.clone(),
-        &mut bitvec_comp,
-        &lzzzz::lz4f::Preferences::default(),
-    )
-    .expect("error on compressing bitvec");
+    // bitvec
 
-    let mut bitvec_file = std::fs::File::create(_path.join("bitvec")).unwrap();
-    bitvec_file.write_all(&bitvec_comp).unwrap();
+    let bitvec_file = std::fs::File::create(_path.join("bitvec")).unwrap();
 
-    bitvec_file.sync_all().unwrap();
+    let mut encoder = EncoderBuilder::new()
+        .level(4)
+        .build(bitvec_file)
+        .expect("cannot create encoder");
+
+    encoder.write_all(&filter.bitvec).unwrap();
+
+    encoder.flush().unwrap();
+
+    // hashes
 
     let mut hashes_file = std::fs::File::create(_path.join("hashes")).unwrap();
-    hashes_file.write_all(&filter.hashes.to_ne_bytes()).unwrap();
+    hashes_file.write_all(&filter.hashes.to_le_bytes()).unwrap();
 
     hashes_file.sync_all().unwrap();
 }
@@ -36,18 +39,21 @@ pub fn write_filter(path: &str, filter: &BloomFilter) {
 pub fn read_filter(path: &str) -> BloomFilter {
     let _path = std::path::Path::new(path).join("filter");
 
-    let mut bitvec_file = std::fs::File::open(_path.join("bitvec")).unwrap();
-    let mut bitvec_read: Vec<u8> = Vec::new();
-    bitvec_file.read_to_end(&mut bitvec_read).unwrap();
+    // bitvec
 
-    let mut bitvec = Vec::new();
-    lzzzz::lz4f::decompress_to_vec(&bitvec_read, &mut bitvec).unwrap();
+    let bitvec_file = std::fs::File::open(_path.join("bitvec")).unwrap();
+    let mut decoder = Decoder::new(bitvec_file).unwrap();
+
+    let mut bitvec: Vec<u8> = Vec::new();
+    decoder.read_to_end(&mut bitvec).unwrap();
+
+    // hashes
 
     let mut hashes_file = std::fs::File::open(_path.join("hashes")).unwrap();
     let mut hashes_read: Vec<u8> = Vec::new();
     hashes_file.read_to_end(&mut hashes_read).unwrap();
 
-    let hashes = i64::from_ne_bytes(hashes_read.try_into().unwrap());
+    let hashes = i64::from_le_bytes(hashes_read.try_into().unwrap());
 
     BloomFilter { bitvec, hashes }
 }
