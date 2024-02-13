249184a09e92d8b343440ec786e513c2851baa19
peeeuzin <canaldopedrinho233@gmail.com>
2024-02-13 16:36:22 -0300

    feat!: block thread when lockfile is locked

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,8 @@ pub use collection::Collection;
 pub use config::{DustDataConfig, StorageConfig};
 
 pub use bson;
-use error::{Error, Result};
+use error::Result;
+use fs2::FileExt;
 use serde::{de::DeserializeOwned, Serialize};
 use std::fmt::Debug;
 use std::fs;
@@ -20,10 +21,10 @@ impl DustData {
     pub fn new(config: config::DustDataConfig) -> Result<Self> {
         fs::create_dir_all(&config.data_path).ok();
 
-        if config.data_path.join(".dustdata-lock").exists() {
-            Err(Error::DatabaseLocked)?;
-        } else {
-            fs::File::create(config.data_path.join(".dustdata-lock")).unwrap();
+        if !config.data_path.join(".dustdata-lock").exists() {
+            let file = fs::File::create(config.data_path.join(".dustdata-lock")).unwrap();
+
+            file.lock_exclusive().unwrap();
         }
 
         Ok(Self { config })
@@ -42,6 +43,8 @@ impl DustData {
 
 impl Drop for DustData {
     fn drop(&mut self) {
-        fs::remove_file(self.config.data_path.join(".dustdata-lock")).unwrap();
+        let file = fs::File::open(self.config.data_path.join(".dustdata-lock")).unwrap();
+
+        file.unlock().unwrap();
     }
 }
