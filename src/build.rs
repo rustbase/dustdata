d54526c65d18cae63e59dbc5bc0139c285626b55
peeeuzin <canaldopedrinho233@gmail.com>
2024-03-24 14:25:32 -0300

    feat: fix not present env build error

--- src/build.rs
+++ src/build.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let run_hooky = std::env::var("RUN_HOOKY").unwrap();
+    let run_hooky = std::env::var("RUN_HOOKY");
 
-    if run_hooky == "true" {
+    if run_hooky == Ok("true".to_owned()) {
         hooky::init(true)
     }
 }
